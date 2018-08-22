60b910696b22a8d13dc3da23c7706a2ab95b8c40
JR <j.romo15@outlook.com>
2018-08-22 10:44:50 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,48 +18,62 @@ firebase.initializeApp(config);
 
 // Create a variable to reference the database
 var database = firebase.database();
+var liveChat = [];
 
 // Initial Values
 
-var chat = "";
-chat = $("#displayed-data").appendchild();
 //  chat button submit
 $("#chatButton").on("click", function(event) {
     // Prevent the page from refreshing
     event.preventDefault();
     //  get input
-    chat = $("#displayed-data").text().trim();
+    var chatInput = $("#name-input").val().trim();
     // change firebase info
-
+    console.log(chatInput)
     database.ref().set({
-        chat: chat
-    });
+        liveChat: chatInput
+    })
+
+    //after sending, clear the chat bar
+    $("#name-input").val("");
+
+
 });
 
 // When changes occurs it will print them to console and html
-database.ref().on("value", function(snapshot) {
+// database.ref().on("value", function(snapshot) {
+//     /*
+//     the value contains the current move 
+//     */
+//     // Print the initial data to the console.
+//     console.log(snapshot.val());
 
-    // Print the initial data to the console.
-    console.log(snapshot.val());
 
-    // Log the value of the various properties
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().age);
-    console.log(snapshot.val().phone);
-
-    // Change the HTML
-    $("#displayed-data").text(snapshot.val().chat
-        //     // If any errors are experienced, log them to console.
-        // },
-        // function(errorObject) {
-        //     console.log("The read failed: " + errorObject.code);
-    )
-});
+//     // Change the HTML
+//     //append the snapshot value to the output
+//     $("#displayed-data").html($("#displayed-data").html() + "<br>" + snapshot.val().chat);
+//     //     // If any errors are experienced, log them to console.
+//     // },
+//     // function(errorObject) {
+//     //     console.log("The read failed: " + errorObject.code);
+// });
 
 
 
-// * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+// // * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
 
-// * Styling and theme are completely up to you. Get Creative!
+// // * Styling and theme are completely up to you. Get Creative!
 
-// * Deploy your assignment to Github Pages.
+// var rock = "r";
+// var paper = "p";
+// var scissors = "s";
+
+// // first user picks then must have picked to let player two pick
+
+
+
+// //three buttons, one for each symbol (imgaes can get click events too!)
+// //when clicked, update the DB with your move
+
+
+// // * Deploy your assignment to Github Pages.$("#displayed-data").text(
